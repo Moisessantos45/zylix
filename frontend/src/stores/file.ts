000be75fe6cf8d5cb935c705a7731947e0d1e3c8
Moisessantos45 <santosxphdz34@gmail.com>
2025-12-12 15:53:03 -0600

    Add Pinia store for managing files, including functions for uploading, deleting, and selecting folders

---
+++ frontend/src/stores/file.ts
@@ -0,0 +1,46 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { GetFolder, RemoveFile } from "../../wailsjs/go/main/App";
+import type { FileItem } from "../types";
+
+const useFileStore = defineStore("file", () => {
+  const fileItems = ref<FileItem[]>([]);
+
+  const handleFilesUploaded = (files: FileItem[]) => {
+    fileItems.value = [...files];
+    console.log("Files uploaded:", fileItems.value);
+  };
+
+  const removeFile = async (id: string) => {
+    const file = fileItems.value.find((item) => item.id === id);
+    if (!file?.id) {
+      return;
+    }
+
+    fileItems.value = fileItems.value.filter((file) => file.id !== id);
+    console.log("File removed. Remaining files:", fileItems.value);
+    try {
+      await RemoveFile(file.path);
+    } catch (error) {
+      console.warn("ocurrrio un error en la eliminacion", error);
+    }
+  };
+
+  const selectFolder = async () => {
+    try {
+      const folderPath = await GetFolder();
+      console.log("Selected folder:", folderPath);
+    } catch (error) {
+      console.error("Error selecting folder:", error);
+    }
+  };
+
+  return {
+    fileItems,
+    handleFilesUploaded,
+    removeFile,
+    selectFolder,
+  };
+});
+
+export default useFileStore;
