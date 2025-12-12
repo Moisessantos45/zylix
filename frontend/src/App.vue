5cc1a70f737002664879eae5967af9c92dc0dcd8
Moisessantos45 <santosxphdz34@gmail.com>
2025-12-12 15:53:33 -0600

    Fix: Update text color in main layout for better readability

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import Header from './components/Header.vue';
 
 <template>
   <Header />
-  <main class="min-h-screen bg-background-light text-[#111813] font-display group/design-root">
+  <main class="min-h-screen bg-background-light text-text-dark font-display group/design-root">
     <RouterView />
   </main>
 </template>
